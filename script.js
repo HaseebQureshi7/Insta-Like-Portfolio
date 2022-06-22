2b1b47855d94b599c84e3d2aa0752b98029fde38
Haseeb <qureshihaxeeb2@gmail.com>
2022-06-22 23:16:32 +0530

    new animation added!

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Social Media Links
-document.querySelector('.linkedin').addEventListener("click", () => window.open("https://www.linkedin.com/in/haseebqureshiishere/", "_self"));
-
 // Loading Animation
 $(window).on('load', function (){
     $('.animation-screen').fadeOut('slow');
 })
+
+// Social Media Links
+document.querySelector('.linkedin').addEventListener("click", () => window.open("https://www.linkedin.com/in/haseebqureshiishere/", "_self"));
 
 document.querySelector('.g-mail').addEventListener("click", () => window.open("mailto:qureshihaxeeb2@gmail.com", "_self"));
 
