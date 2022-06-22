646dcf0709e73b9b5bf0bd321c27c3c7ea84e7d5
Haseeb <qureshihaxeeb2@gmail.com>
2022-06-22 20:16:02 +0530

    new features added!

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ document.querySelector('.linkedin').addEventListener("click", () => window.open(
 document.querySelector('.g-mail').addEventListener("click", () => window.open("mailto:qureshihaxeeb2@gmail.com", "_self"));
 
 document.querySelector('.instagram').addEventListener("click", () => window.open("https://www.instagram.com/haseebxqureshi/", "_self"));
+
+// Github Link 
+document.querySelector('.posts6').addEventListener("click", () => window.open("https://github.com/HaseebQureshi7", "_self"));
+
+
+// For Testing Only
+document.querySelector('.posts1').addEventListener("click", () => window.location.href = "posts/post-view.html");
+
