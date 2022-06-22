dc1966a609713d7aed01a89dbd5cd41a752fa787
Haseeb <qureshihaxeeb2@gmail.com>
2022-06-22 19:17:38 +0530

    links added!

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("its Working");
+// Social Media Links
+document.querySelector('.linkedin').addEventListener("click", () => window.open("https://www.linkedin.com/in/haseebqureshiishere/", "_self"));
+
+document.querySelector('.g-mail').addEventListener("click", () => window.open("mailto:qureshihaxeeb2@gmail.com", "_self"));
+
+document.querySelector('.instagram').addEventListener("click", () => window.open("https://www.instagram.com/haseebxqureshi/", "_self"));
